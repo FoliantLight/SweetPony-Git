8b8c5e13691abdebc7efa3b0a01b8970a1bb0843
FoliantLight <foliant.light@yandex.ru>
2016-05-19 23:35:54 +0300

    Добавлена основа для взаимодействий
    - тег "ActionItem"
    - скрипт "ActionItem"
    - скрипт "MainPersonActionManager"

---
+++ Assets/FoliantLight/GameScripts/MainPersonActionController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MainPersonActionController : MonoBehaviour {
+
+    private GameObject actionItem;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (actionItem != null)
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Use"))
+            Debug.Log(actionItem.transform.tag);
+        }
+	}
+
+    public void isUseAroundEnter(GameObject go)
+    {
+        actionItem = go;
+    }
+
+    public void isUseAroundExit()
+    {
+        actionItem = null;
+    }
+}
