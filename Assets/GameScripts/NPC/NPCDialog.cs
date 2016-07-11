2640d3be20adb9289f1664cdbeefb88da90072e5
Liz.Lotova <lizlotova@gmail.com>
2016-07-11 22:33:09 +0400

    Часть класса НИПа, диалог, сцена. Нет вывода в юнити и боевых действий

---
+++ Assets/GameScripts/NPC/NPCDialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Assets.NPC
+{
+    public class NPCDialog
+    {
+        /// <summary>
+        /// Узел XML, содержащий записи диалога
+        /// </summary>
+        XmlNode Root;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Root">Узел XML, содержащий записи диалога</param>
+        public NPCDialog(XmlNode Root)
+        {
+            this.Root = Root;
+        }
+
+        /// <summary>
+        /// Получение нового вопроса после выбора одного из ответов
+        /// </summary>
+        /// <param name="QuestionNumber">Номер вопроса, например 1.1</param>
+        /// <param name="AnswerIndex">Номер ответа на вопрос</param>
+        /// <returns></returns>
+        public NPCEntry GetEntry(string QuestionNumber, int AnswerIndex)
+        {
+            var node = Root.SelectSingleNode("entry[@id='"
+                + QuestionNumber
+                + (AnswerIndex == 0 ? "" : "." + AnswerIndex)
+                + "']");
+            var entry = new NPCEntry(node);
+            return entry;
+        }
+
+    }
+
+}
---
+++ Assets/GameScripts/NPC/NPCTestButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.NPC
+{
+    public class NPCTestButtonScript : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void onClick()
+        {
+            var npc = new NPC("test");
+        }
+    }
+}
