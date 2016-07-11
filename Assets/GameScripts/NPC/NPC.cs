996bc498d52a543df698aba7bff68f6a7c6bef6a
Liz.Lotova <lizlotova@gmail.com>
2016-07-11 22:33:09 +0400

    Часть класса НИПа, диалог. Нет вывода в юнити

---
+++ Assets/GameScripts/NPC/NPC.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Xml;
+
+namespace Assets.NPC
+{
+    public class NPC
+    {
+        /// <summary>
+        /// Имя нипа. Определяет xml с диалогом 
+        /// и другие файлы для нипа, если понадобится
+        /// </summary>
+        private string Name;
+        /// <summary>
+        /// Активные - дают квесты и учавствуют в диалогах
+        /// Пассивные - не дают заданий, не общаются
+        /// </summary>
+        public bool isActive = true;
+        /// <summary>
+        /// Начальная позиция нипа. Его дом. 
+        /// </summary>
+        public Vector3 startPosition;
+        /// <summary>
+        /// Начальная позиция нипа случайна
+        /// Для нипов-путешественников, например
+        /// </summary>
+        public bool isRandomStartPosition = false;
+        /// <summary>
+        /// Ночные нипы не возвращаются домой для сна
+        /// </summary>
+        public bool isNight = true;
+        /// <summary>
+        /// Диалог с НИПом
+        /// </summary>
+        NPCDialog Dialog;
+        /// <summary>
+        /// Путь для папки с xml
+        /// </summary>
+        string Path = "Assets/GameScripts/NPC/";
+
+
+
+        // как-то надо указать внешний вид:
+        // либо файл с изображением name.png (или несколько для анимации)
+        // либо указание цвета и других ништяков
+        // тогда нужно наследование от MainPerson
+
+        /// <summary>
+        /// </summary>
+        /// <param name="Name">Имя НИПа и xml файла с его параметрами</param>
+        public NPC(string Name)
+        {
+            this.Name = Name;
+            // TODO: расшифровка
+
+            var doc = new XmlDocument();
+            doc.Load(Path + Name + ".xml");
+            var root = doc.DocumentElement;
+
+            // TODO: загрузка остальных параметров
+            Dialog = new NPCDialog(
+                root.GetElementsByTagName("dialog").Item(0));
+
+
+          //  Dialog.GetEntry("1", 0);
+          //  Dialog.GetEntry("1", 2);
+        }
+
+
+        /// <summary>
+        /// Функция, связанная с юнити
+        /// </summary>
+        public void ShowCurrentEntry()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/GameScripts/NPC/NPCEntry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Assets.NPC
+{ 
+    /// <summary>
+    /// Диалоговая запись
+    /// Вопрос NPC и варианты ответа MainPerson
+    /// </summary>
+    public class NPCEntry
+    {
+        // new NPCEntry(){Question = "123", Answer = new List<string>(){"a","b"}};
+       
+        /// <summary>
+        /// Вопрос NPC
+        /// </summary>
+        public string Question;
+
+        /// <summary>
+        /// Номер вопроса. 
+        /// Номер ответа генерируется так:
+        /// Number + "." + выбранный номер в списке ответов.
+        /// Нумерация в списке ответов с нуля.
+        /// </summary>
+        public string Number;
+
+        /// <summary>
+        /// Список возможных ответов. 
+        /// Может быть пустым.
+        /// </summary>
+        public List<string> Answers;
+
+        /// <summary>
+        /// Создание диалоговой записи - вопроса и ответов из строк
+        /// </summary>
+        /// <param name="Question">Вопрос NPC</param>
+        /// <param name="Number">Номер вопроса. Пример: 1.3.2</param>
+        /// <param name="Answers">Несколько вариантов ответа</param>
+        public NPCEntry(string Question, string Number, params string[] Answers)
+        {
+            this.Question = Question;
+            this.Number = Number;
+            this.Answers = Answers.ToList<string>();
+        }
+
+
+        public NPCEntry(XmlNode xml)
+        {
+            Question = xml.Attributes.GetNamedItem("q").Value;
+            Number = xml.Attributes.GetNamedItem("id").Value;
+            Answers = new List<string>();
+            foreach (XmlNode ch in xml.ChildNodes)
+                Answers.Add(ch.InnerText);
+        }        
+    }
+}
