5220de3a11b2b609f74a70f87d5ed3e91b1683fe
FoxyShadow <georgeosipov52@yahoo.com>
2016-09-04 02:54:05 +0300

    Inventory fix

--- Assets/GameScripts/Inventory/ItemPanel.cs
+++ Assets/GameScripts/Inventory/ItemPanel.cs
@@ -4,20 +4,19 @@ using UnityEngine.EventSystems;
 
 public class ItemPanel : MonoBehaviour, IDropHandler {
     private InventoryPanel m_inventoryPanel;
-    private Inventory m_newInventory;
 
     void Start() {
         m_inventoryPanel = transform.parent.GetComponent<InventoryPanel>();
-        m_newInventory = m_inventoryPanel.inventory;
     }
 
     public void OnDrop(PointerEventData eventData) {
         InventoryIcon icon = eventData.pointerDrag.GetComponent<InventoryIcon>();
         Inventory oldInventory = icon.inventoryPanel.inventory;
+        Inventory newInventory = m_inventoryPanel.inventory;
         Vector2Int oldCoordinates = oldInventory.inventoryPanel.panelCoordinates(icon.oldParent.GetComponent<ItemPanel>());
         Vector2Int newCoordinates = m_inventoryPanel.panelCoordinates(this);
 
-        if(!m_newInventory.takeOnly && m_newInventory.addItem(newCoordinates, icon.info, false)) {
+        if(!newInventory.takeOnly && newInventory.addItem(newCoordinates, icon.info, false)) {
             icon.transform.SetParent(transform);
         }
         else {
